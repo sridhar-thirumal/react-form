f5a1af81852937ebc8b3fd024b8801811344749e
sridhar-thirumal <sridhar.thirumal@bigthinkcode.com>
2025-06-25 12:32:46 +0530

    Seperated components

---
+++ signup-page/src/components/InputField.jsx
@@ -0,0 +1,12 @@
+function InputField(props) {
+    return(
+        <div className='flex flex-col gap-y-1 sm:grid sm:grid-cols-[10rem_12rem_2rem]'>
+          <label htmlFor={props.id}>{props.name}: </label>
+          <input type='text' id={props.id} className='border-1 border-black max-w-[12rem]' required
+          onChange={props.onChange} value={props.value}/>
+          {!props.valid && <p className='text-[#fc0303] text-sm sm:col-start-2'>{props.message}</p>}
+        </div>
+    );
+}
+
+export default InputField;
---
+++ signup-page/src/components/PasswordField.jsx
@@ -0,0 +1,24 @@
+function PasswordField(props) {
+    return (
+        <div className='flex flex-col gap-y-1 sm:grid sm:grid-cols-[10rem_12rem_2rem]'>
+          <label htmlFor={props.id}>{props.name}: </label>
+          <div className='flex flex-row'>
+            <input id={props.id} type={props.type} className='border-1 border-black max-w-[12rem]' required
+            onChange={props.onChange} value={props.value}/>
+            <button onClick={props.toggle} className='ml-2 sm:ml-2'>
+              {props.type === "password" ? 
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye-off-icon lucide-eye-off">
+                <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49"/><path d="M14.084 14.158a3 3 0 0 1-4.242-4.242"/><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143"/>
+                <path d="m2 2 20 20"/></svg>
+                :
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye-icon lucide-eye">
+                <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/>
+                <circle cx="12" cy="12" r="3"/></svg>}
+            </button>
+            </div>
+          {!props.valid && <p className='text-[#fc0303] text-sm sm:col-start-2'>{props.message}</p>}
+        </div>
+    );
+}
+
+export default PasswordField;
